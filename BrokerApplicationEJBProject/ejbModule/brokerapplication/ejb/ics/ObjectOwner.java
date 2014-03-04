eaef185ab024633a4fbaf3b3b29c0cca7d8e1ebf
MarcusJacobsson <mackan07@hotmail.com>
2014-03-04 12:28:44 +0100

    start

--- BrokerApplicationEJBProject/ejbModule/brokerapplication/ejb/ics/ObjectOwner.java
+++ BrokerApplicationEJBProject/ejbModule/brokerapplication/ejb/ics/ObjectOwner.java
@@ -1,5 +1,53 @@
 package brokerapplication.ejb.ics;
 
-public class ObjectOwner {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "ObjectOwner")
+public class ObjectOwner {
+	
+	private String ownerSsnr;
+	private String name;
+	private String phoneNr;
+	private String email;
+
+	@Id
+	@Column(name="ownerSsnr")
+	public String getOwnerSsnr() {
+		return ownerSsnr;
+	}
+
+	public void setOwnerSsnr(String ownerSsnr) {
+		this.ownerSsnr = ownerSsnr;
+	}
+
+	@Column(name="name")
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Column(name="phoneNr")
+	public String getPhoneNr() {
+		return phoneNr;
+	}
+
+	public void setPhoneNr(String phoneNr) {
+		this.phoneNr = phoneNr;
+	}
+
+	@Column(name="email")
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 }
--- BrokerApplicationEJBProject/ejbModule/brokerapplication/ejb/ics/Showing.java
+++ BrokerApplicationEJBProject/ejbModule/brokerapplication/ejb/ics/Showing.java
@@ -1,5 +1,37 @@
 package brokerapplication.ejb.ics;
 
-public class Showing {
+import java.io.Serializable;
 
+import javax.persistence.Entity;
+
+@Entity
+public class Showing implements Serializable {
+	
+	private int objNr;
+	private String buyerSsnr;
+	private String showingDate;
+
+	public int getObjNr() {
+		return objNr;
+	}
+
+	public void setObjNr(int objNr) {
+		this.objNr = objNr;
+	}
+
+	public String getBuyerSsnr() {
+		return buyerSsnr;
+	}
+
+	public void setBuyerSsnr(String buyerSsnr) {
+		this.buyerSsnr = buyerSsnr;
+	}
+
+	public String getShowingDate() {
+		return showingDate;
+	}
+
+	public void setShowingDate(String showingDate) {
+		this.showingDate = showingDate;
+	}
 }
