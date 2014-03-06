db01d1eeae4b0dda7b00413c80912ab0237b4da2
MarcusJacobsson <mackan07@hotmail.com>
2014-03-06 10:30:43 +0100

    servlet

--- BrokerApplicationClientProject/src/org/ics/ejb/brokerapplication/BrokerApplicationServlet.java
+++ BrokerApplicationClientProject/src/org/ics/ejb/brokerapplication/BrokerApplicationServlet.java
@@ -1,6 +1,8 @@
 package org.ics.ejb.brokerapplication;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,20 +15,34 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/BrokerApplicationServlet")
 public class BrokerApplicationServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public BrokerApplicationServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public BrokerApplicationServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void service(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+
+		PrintWriter out = response.getWriter();
+
+		out.println("<!DOCTYPE html><html><head>");
+		out.println("<title>Lab1</title>");
+		out.println("<meta charset=\"ISO-8859-1\">");
+		out.println("</head><body>");
+
+		out.println("<h2>ObjectOwner</h2>");
+
+		out.println("</body></html>");
+
 	}
 
 }
